5ef862b39bcf2287d58e5fdabd620875201a1aeb
Роман Соколов <sokoloff.rv@gmail.com>
2023-04-21 12:15:54 +0600

    Удалены родные миграции, обновлена миграция users

--- database/migrations/2023_04_20_212903_create_users_table.php
+++ database/migrations/2023_04_20_212903_create_users_table.php
@@ -10,9 +10,14 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->string('avatar')->nullable();
             $table->string('role')->default('user');
+            $table->timestamps();
         });
     }
 
