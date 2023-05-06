0ec79b85bf1e6cf250a82d05b1bf30df33675ca7
Роман Соколов <sokoloff.rv@gmail.com>
2023-05-06 16:30:02 +0600

    Исправления кодстайла

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,6 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
