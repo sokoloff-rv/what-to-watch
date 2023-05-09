ab4e471dd171440be2ff83cf98a56670c8267fd8
Роман Соколов <sokoloff.rv@gmail.com>
2023-05-09 22:31:55 +0600

    API-ключ от Omdb перенесен в .env файл

--- app/Services/MovieService/MovieOmdbRepository.php
+++ app/Services/MovieService/MovieOmdbRepository.php
@@ -7,12 +7,13 @@ use GuzzleHttp\Client;
 class MovieOmdbRepository implements MovieRepositoryInterface
 {
     private Client $client;
-    private string $apiKey = 'a471b1ee';
+    private string $apiKey;
     private string $baseUrl = 'http://www.omdbapi.com/';
 
     public function __construct(Client $client)
     {
         $this->client = $client;
+        $this->apiKey = config('services.omdb.api_key');
     }
 
     public function findMovieById(string $imdbId): ?array
