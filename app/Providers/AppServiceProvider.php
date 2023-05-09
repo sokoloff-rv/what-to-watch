8b9fe3c60e5b45f9d20989771c72c72a4be8867b
Роман Соколов <sokoloff.rv@gmail.com>
2023-05-10 00:03:28 +0600

    Добавлен репозиторий Академии

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\MovieService\MovieRepositoryInterface;
 use App\Services\MovieService\MovieOmdbRepository;
+use App\Services\MovieService\MovieAcademyRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(MovieRepositoryInterface::class, MovieOmdbRepository::class);
+        // $this->app->bind(MovieRepositoryInterface::class, MovieOmdbRepository::class);
+        $this->app->bind(MovieRepositoryInterface::class, MovieAcademyRepository::class);
     }
 
     /**
