4a27d40f56aa82da04639a431434acb359ecb1f2
Роман Соколов <sokoloff.rv@gmail.com>
2023-05-14 19:33:37 +0600

    Внесены исправления в репозитории

--- app/Services/MovieService/MovieAcademyRepository.php
+++ app/Services/MovieService/MovieAcademyRepository.php
@@ -2,14 +2,14 @@
 
 namespace App\Services\MovieService;
 
-use Illuminate\Support\Facades\Http;
 use App\DTO\FilmData;
+use Illuminate\Support\Facades\Http;
 
 class MovieAcademyRepository implements MovieRepositoryInterface
 {
     private string $baseUrl = 'http://guide.phpdemo.ru/api/films/';
 
-    public function findMovieById(string $imdbId): ?FilmData
+    public function findMovieById(string $imdbId): ?array
     {
         $response = Http::get($this->baseUrl . $imdbId);
 
@@ -20,6 +20,7 @@ class MovieAcademyRepository implements MovieRepositoryInterface
         $filmData->poster_image = $movieData['poster'];
         $filmData->preview_image = $movieData['icon'];
         $filmData->background_image = $movieData['background'];
+        $filmData->background_color = null;
         $filmData->video_link = $movieData['video'];
         $filmData->preview_video_link = $movieData['preview'];
         $filmData->description = $movieData['desc'];
@@ -29,7 +30,10 @@ class MovieAcademyRepository implements MovieRepositoryInterface
         $filmData->imdb_id = $movieData['imdb_id'];
         $filmData->starring = $movieData['actors'];
         $filmData->genre = $movieData['genres'];
+        $filmData->rating = null;
+        $filmData->scores_count = null;
 
-        return $filmData;
+        return $filmData->toArray();
     }
+
 }
--- app/Services/MovieService/MovieOmdbRepository.php
+++ app/Services/MovieService/MovieOmdbRepository.php
@@ -17,7 +17,7 @@ class MovieOmdbRepository implements MovieRepositoryInterface
         $this->apiKey = config('services.omdb.api_key');
     }
 
-    public function findMovieById(string $imdbId): ?FilmData
+    public function findMovieById(string $imdbId): ?array
     {
         $response = $this->client->request('GET', $this->baseUrl, [
             'query' => [
@@ -31,17 +31,22 @@ class MovieOmdbRepository implements MovieRepositoryInterface
         $filmData = new FilmData();
         $filmData->name = $movieData['Title'];
         $filmData->poster_image = $movieData['Poster'];
+        $filmData->preview_image = null;
+        $filmData->background_image = null;
+        $filmData->background_color = null;
+        $filmData->video_link = null;
+        $filmData->preview_video_link = null;
         $filmData->description = $movieData['Plot'];
         $filmData->director = $movieData['Director'];
         $filmData->released = (int) $movieData['Year'];
         $filmData->run_time = (int) $movieData['Runtime'];
-        $filmData->rating = (float) $movieData['imdbRating'];
-        $filmData->scores_count = (int) str_replace(',', '', $movieData['imdbVotes']);
         $filmData->imdb_id = $movieData['imdbID'];
         $filmData->starring = array_map('trim', explode(',', $movieData['Actors']));
         $filmData->genre = array_map('trim', explode(',', $movieData['Genre']));
+        $filmData->rating = (float) $movieData['imdbRating'];
+        $filmData->scores_count = (int) str_replace(',', '', $movieData['imdbVotes']);
 
-        return $filmData;
+        return $filmData->toArray();
     }
 
 }
