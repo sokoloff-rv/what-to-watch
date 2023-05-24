be76ec08eed04875263223df77c46667030b7c7e
Роман Соколов <sokoloff.rv@gmail.com>
2023-05-24 21:12:37 +0600

    Исправления кодстайла

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new UpdateCommentsForAllFilmJob)->daily();
+        $schedule->job(new UpdateCommentsForAllFilmJob())->daily();
     }
 
     /**
